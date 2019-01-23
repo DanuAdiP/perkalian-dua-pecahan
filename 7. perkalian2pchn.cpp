73177f2e19a309e44792ec6a320241bfc5908f8a
DanuAdiP <danu261216@gmail.com>
2019-01-23 21:28:49 +0700

    Commit Pertama

---
+++ 7. perkalian2pchn.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int pb,py;
+	cout<<"PERKALIAN 2 BILANGAN PECAHAN"<<endl;
+	cout<<"Masukkan pb = ";cin>>pb;
+	cout<<"Masukkan py = ";cin>>py;
+	cout<<"       "<<pb;cout<<endl;
+	cout<<"  P1 = -"<<endl;
+	cout<<"       "<<py;
+	cout<<endl;
+	cout<<"Masukkan pb = ";cin>>pb;
+	cout<<"Masukkan py = ";cin>>py;
+	cout<<"       "<<pb;cout<<endl;
+	cout<<"  P2 = -"<<endl;
+	cout<<"       "<<py;	
+}
